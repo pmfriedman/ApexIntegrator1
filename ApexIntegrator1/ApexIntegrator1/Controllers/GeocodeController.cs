ecc66f79e3bcabadee7c687fa430afdfe4adb0b2
Philip Friedman <p.friedman@outlook.com>
2016-04-17 21:08:41 -0400

    jquery for geocode

---
+++ ApexIntegrator1/ApexIntegrator1/Controllers/GeocodeController.cs
@@ -0,0 +1,30 @@
+﻿using ApexServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization;
+using ApexServices.Apex;
+
+namespace ApexIntegrator1.Controllers
+{
+    public class GeocodeController : Controller
+    {
+        // GET: Geocode
+        public ActionResult Index(string result)
+        {
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DoGeocode(string text)
+        {
+            var service = new GeocodeService();
+            var geocode = await service.GeocodeAsync(text);
+
+            return Json(geocode?.ResultsData?[0]?.Description);
+        }
+    }
+}
