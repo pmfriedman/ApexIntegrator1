f2fd4c1e667a01365ce6a0018f0465b7e770fcbd
Philip Friedman <p.friedman@outlook.com>
2016-04-19 08:10:30 -0400

    Default Entities

--- ApexIntegrator1/ApexServices/DefaultEntities.cs
+++ ApexIntegrator1/ApexServices/DefaultEntities.cs
@@ -23,8 +23,95 @@ namespace ApexServices
         {
             if (_sharedDefaults == null)
             {
+                _sharedDefaults = new DefaultEntities();
+
                 var connection = await Connection.GetConnectionAsync();
 
+                // service time type
+
+                var svcTimeType = new ServiceTimeType
+                {
+                    Action = ActionType.Add,
+                    Identifier = "APEX_INTEGRATOR_DEF"
+                };
+
+                var queryResult = await connection.Query.RetrieveAsync(
+                    connection.Session,
+                    connection.RegionContext,
+                    new RetrievalOptions
+                    {
+                        Expression = new EqualToExpression
+                        {
+                            Left = new PropertyExpression { Name = "Identifier" },
+                            Right = new ValueExpression { Value = svcTimeType.Identifier }
+                        },
+                        PropertyInclusionMode = PropertyInclusionMode.All,
+                        Type = typeof(ServiceTimeType).Name,
+                    });
+
+                if (queryResult.RetrieveResult.Items.Length > 0)
+                {
+                    _sharedDefaults.ServiceTimeTypeEntityKey = 
+                        queryResult.RetrieveResult.Items.OfType<ServiceTimeType>().First().EntityKey;
+                }
+                else
+                {
+                    var saveResult =
+                        await connection.Routing.SaveAsync(
+                            connection.Session,
+                            connection.RegionContext,
+                            new[] { svcTimeType },
+                            new SaveOptions
+                            {
+                                InclusionMode = PropertyInclusionMode.All,
+                                ReturnInclusionMode = PropertyInclusionMode.None,
+                                ReturnSavedItems = false
+                            });
+                    _sharedDefaults.ServiceTimeTypeEntityKey = saveResult.SaveResult.First().EntityKey;
+                }
+
+                // time window type
+
+                var twType = new TimeWindowType
+                {
+                    Action = ActionType.Add,
+                    Identifier = "APEX_INTEGRATOR_DEF"
+                };
+
+                queryResult = await connection.Query.RetrieveAsync(
+                    connection.Session,
+                    connection.RegionContext,
+                    new RetrievalOptions
+                    {
+                        Expression = new EqualToExpression
+                        {
+                            Left = new PropertyExpression { Name = "Identifier" },
+                            Right = new ValueExpression { Value = twType.Identifier }
+                        },
+                        PropertyInclusionMode = PropertyInclusionMode.All,
+                        Type = typeof(TimeWindowType).Name,
+                    });
+
+                if (queryResult.RetrieveResult.Items.Length > 0)
+                {
+                    _sharedDefaults.ServiceTimeTypeEntityKey =
+                        queryResult.RetrieveResult.Items.OfType<ServiceTimeType>().First().EntityKey;
+                }
+                else
+                {
+                    var saveResult =
+                        await connection.Routing.SaveAsync(
+                            connection.Session,
+                            connection.RegionContext,
+                            new[] { twType },
+                            new SaveOptions
+                            {
+                                InclusionMode = PropertyInclusionMode.All,
+                                ReturnInclusionMode = PropertyInclusionMode.None,
+                                ReturnSavedItems = false
+                            });
+                    _sharedDefaults.TimeWindowTypeEntityKey = saveResult.SaveResult.First().EntityKey;
+                }
 
 
             }
---
+++ ApexIntegrator1/ApexServicesTest/DefaultEntitiesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ApexServices;
+
+namespace ApexServicesTest
+{
+    [TestFixture]
+    public class DefaultEntitiesTests
+    {
+        [Test]
+        public async Task GetDefaults()
+        {
+            var defaults = await DefaultEntities.GetDefaultsAsync();
+
+            Assert.That(defaults.ServiceTimeTypeEntityKey, Is.GreaterThan(0));
+            Assert.That(defaults.TimeWindowTypeEntityKey, Is.GreaterThan(0));
+        }
+    }
+}
