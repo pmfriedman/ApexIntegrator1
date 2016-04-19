14531eff01fc9a6e8c0e98fb9e5fc89b05d54acd
Philip Friedman <p.friedman@outlook.com>
2016-04-18 22:32:12 -0400

    Order Service

---
+++ ApexIntegrator1/ApexServices/OrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApexServices.Apex;
+
+namespace ApexServices
+{
+    public class OrderService
+    {
+        public async Task<Order> CreateOrder(string address)
+        {
+            var geocoder = new GeocodeService();
+            var geocode = await geocoder.GeocodeAsync(address);
+
+        }
+    }
+}
