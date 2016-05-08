6567e24589bac4cad65200a012b4d064738c44dd
Philip Friedman <p.friedman@outlook.com>
2016-05-08 16:07:17 -0400

    Update Location Address

--- ApexIntegrator1/ApexServices/LocationService.cs
+++ ApexIntegrator1/ApexServices/LocationService.cs
@@ -22,7 +22,14 @@ namespace ApexServices
                 StandingPickupQuantities = new Quantities(),
                 TimeWindowTypeEntityKey = defaults.TimeWindowTypeEntityKey,
                 ServiceTimeTypeEntityKey = defaults.ServiceTimeTypeEntityKey,
-                Address = new Address()
+                Address = new Address
+                {
+                    Locality = new Locality
+                    {
+                        CountryISO3Abbr = "USA"
+                    }
+                },
+                WorldTimeZone_TimeZone = WorldTimeZone.EasternTimeUSCanada.ToString()
             };
 
             var result = await connection.Routing.SaveAsync(
@@ -39,6 +46,67 @@ namespace ApexServices
             var key = result.SaveResult[0].EntityKey;
 
             return key;
+        }
+
+        public async Task<long> UpdateLocationAddress(long locationEntityKey, long version, string addressText)
+        {
+            var geocode = (await new GeocodeService().GeocodeAsync(addressText)).ResultsData[0];
+
+            var location = new ServiceLocation()
+            {
+                Action = ActionType.Update,
+                EntityKey = locationEntityKey,
+                Version = version,
+                Description = geocode.Description,
+                Coordinate = new Coordinate
+                {
+                    Latitude = geocode.Location.Latitude,
+                    Longitude = geocode.Location.Longitude
+                }
+            };
+
+            var propOptions = new ServiceLocationPropertyOptions
+            {
+                Coordinate = true,
+                Description = true
+            };
+
+            var connection = await Connection.GetConnectionAsync();
+            var result = await connection.Routing.SaveAsync(
+                connection.Session,
+                connection.RegionContext,
+                new[] { location },                
+                new SaveOptions
+                {
+                    InclusionMode = PropertyInclusionMode.AccordingToPropertyOptions,
+                    ReturnInclusionMode = PropertyInclusionMode.None,
+                    ReturnSavedItems = false,
+                    PropertyOptions = propOptions
+                });
+
+            var key = result.SaveResult[0].EntityKey;
+
+            return key;
+        }
+
+        public async Task<ServiceLocation> GetFullServiceLocation(long entityKey)
+        {
+            var connection = await Connection.GetConnectionAsync();
+            var locationResult = await connection.Query.RetrieveAsync(
+                connection.Session,
+                connection.RegionContext,
+                new RetrievalOptions
+                {
+                    Type = typeof(ServiceLocation).Name,
+                    PropertyInclusionMode = PropertyInclusionMode.All,
+                    Expression = new EqualToExpression
+                    {
+                        Left = new PropertyExpression { Name = "EntityKey" },
+                        Right = new ValueExpression { Value = entityKey }
+                    }
+                });
+
+            return locationResult.RetrieveResult.Items[0] as ServiceLocation;
         }
     }
 }
--- ApexIntegrator1/ApexServicesTest/LocationTests.cs
+++ ApexIntegrator1/ApexServicesTest/LocationTests.cs
@@ -19,5 +19,21 @@ namespace ApexServicesTest
 
             Assert.That(key, Is.GreaterThan(0));
         }
+
+        [Test]
+        public async Task UpdateLocationAddress()
+        {
+            var service = new LocationService();
+            var key = await service.CreateNewLocation();
+            
+            var location = await service.GetFullServiceLocation(key);
+            Assert.That(location.Identifier, Is.Not.Empty);
+            Assert.That(location.Coordinate.Latitude, Is.EqualTo(0));
+
+            await service.UpdateLocationAddress(key, location.Version, "2807 LAURELWOOD CT, BALTIMORE, BALTIMORE CITY, MD, USA 21209");
+            
+            location = await service.GetFullServiceLocation(key);            
+            Assert.That(location.Coordinate.Latitude, Is.Not.EqualTo(0));
+        }
     }
 }
